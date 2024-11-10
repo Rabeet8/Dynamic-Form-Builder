ba6afcb5f45739a454ff990ad83d039d1ba29cca
Syed Rabeet-Ur-Rehman <92630941+Rabeet8@users.noreply.github.com>
2024-11-11 01:05:15 +0500

    Task Finalized

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import FormBuilder from "./Components/FormBuilder";
 const App = () => {
   return (
     <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "10vh",
+          margin: 0,
+        }}
+      >
+        <h1
+          style={{
+            fontFamily: "Poppins, sans-serif",
+            margin: "20px 0",
+            textAlign: "center",
+          }}
+        >
+          Dynamic Form Builder - Task (Plutus21)
+        </h1>
+      </div>
       <FormBuilder />
     </div>
   );
